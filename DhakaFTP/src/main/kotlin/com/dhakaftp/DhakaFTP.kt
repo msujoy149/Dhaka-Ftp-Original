887c1b5d68f906225c42f63222950142ff7d163f
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-03 13:12:30 +0600

    Update DhakaFTP.kt

--- DhakaFTP/src/main/kotlin/com/dhakaftp/DhakaFTP.kt
+++ DhakaFTP/src/main/kotlin/com/dhakaftp/DhakaFTP.kt
@@ -2,7 +2,9 @@ package com.dhakaftp
 
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
+@CloudstreamPlugin
 class DhakaFTP : MainAPI() {
 
     override var name = "Dhaka FTP"
