4a269712b9155f364ea00999a50047bad2deffa4
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-03 11:13:17 +0600

    Update mainUrl and name for ExampleProvider

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -5,8 +5,8 @@ import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.TvType
 
 class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
+    override var mainUrl = "http://172.16.50.7/" 
+    override var name = "Dhaka FTP Original"
     override val supportedTypes = setOf(TvType.Movie)
 
     override var lang = "en"
